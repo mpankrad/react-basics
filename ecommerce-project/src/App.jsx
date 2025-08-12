500415c7f32a1566342f67cf959d591d2377f760
mpankrad <kolia.pankrad@gmail.com>
2025-08-12 08:50:18 +0300

    created multiple pages, using Route

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,16 @@
+import {Routes, Route} from 'react-router';
+
 import { HomePage } from './pages/Home-page'
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage/>
+    <Routes>
+      <Route path index element = {<HomePage/>} />   
+      <Route path ="checkout" element = {<div>test</div>} /> 
+    </Routes>
+    
   )
 }
 

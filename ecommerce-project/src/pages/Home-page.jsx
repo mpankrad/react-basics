931ee15eb3ebf02de70017fa564ad4e8adac439c
mpankrad <kolia.pankrad@gmail.com>
2025-08-13 14:02:49 +0300

    added different images for the pages favicon

--- ecommerce-project/src/pages/Home-page.jsx
+++ ecommerce-project/src/pages/Home-page.jsx
@@ -6,6 +6,7 @@ export function HomePage() {
     <>
       <Header />
       <title>Online-Shop</title>
+      <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
 
       <div className="home-page">
         <div className="products-grid">
--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -6,6 +6,7 @@ export function OrdersPage () {
     <>
 
       <title>Orders</title>
+      <link rel="icon" type="image/svg+xml" href="/orders-favicon.png" />
 
     <Header />
 

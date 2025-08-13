99c08255256473153465842efa353d44fb1f12d7
mpankrad <kolia.pankrad@gmail.com>
2025-08-13 14:33:58 +0300

    Added tracking page into react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -6,6 +6,7 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 
 import './App.css'
+import { Tracking } from './pages/Tracking';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Route index element = {<HomePage />} />   
       <Route path ="checkout" element = {<CheckoutPage />} /> 
       <Route path ="orders" element = {<OrdersPage/>}></Route>
+      <Route path ="tracking" element = {<Tracking/>}></Route>
     </Routes>
     
   )
